45d8fdd4e9d2997bb84ad9c572dead2f47449aed
Adrian Gutierrez <agd.vcl@gmail.com>
2021-07-05 00:08:14 -0400

    Table for products managment

--- src/app/modules/products/products.module.ts
+++ src/app/modules/products/products.module.ts
@@ -5,9 +5,10 @@ import { ProductsRoutingModule } from './products-routing.module';
 import { ProductContainerComponent } from './product-container/product-container.component';
 import { NgbPaginationModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ProductsManagerComponent } from './products-manager/products-manager.component';
 
 @NgModule({
-  declarations: [ProductsListComponent, ProductContainerComponent],
+  declarations: [ProductsListComponent, ProductContainerComponent, ProductsManagerComponent],
   imports: [
     CommonModule,
     ProductsRoutingModule,
