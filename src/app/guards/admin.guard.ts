ca14fb1957d1bf3a7447fa01dde28f9b8b93a038
Adrian Gutierrez <agd.vcl@gmail.com>
2021-07-04 23:18:55 -0400

    Protecting admin routes

---
+++ src/app/guards/admin.guard.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Authorization } from '../store/authorization.store';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminGuard implements CanActivate {
+  constructor(
+    private authState: Authorization
+  ) {}
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.authState.isAdmin;
+  }
+  
+}
--- src/app/modules/products/products-routing.module.ts
+++ src/app/modules/products/products-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AdminGuard } from 'src/app/guards/admin.guard';
 import { ProductsListComponent } from './products-list/products-list.component';
+import { ProductsManagerComponent } from './products-manager/products-manager.component';
 
 
 const routes: Routes = [
-  { path: '', component: ProductsListComponent }
+  { path: '', component: ProductsListComponent },
+  { path: 'manager', component: ProductsManagerComponent, canActivate: [AdminGuard] }
 ];
 
 @NgModule({
